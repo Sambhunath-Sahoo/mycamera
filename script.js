e9922f8c51144b9183030f5737b9e68335b25810
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-09 10:29:25 +0530

    added buttons ui(recoring and capture)

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ let constraints = {
 
 // navigator is a global object which contains the info of browser
 // The MediaDevices.getUserMedia() method prompts the user for permission to use a media input
-navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-        video.srcObject= stream;
+// navigator.mediaDevices.getUserMedia(constraints)
+//     .then((stream) => {
+//         video.srcObject= stream;
+
+//     })
+//     .catch(function (err) {
         
-    })
-    .catch(function (err) {
-        
-    });
+//     });
+
