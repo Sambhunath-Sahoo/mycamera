9bba6a03b2b37ea03586fafa951f2c43e6c8ed46
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-09 11:43:56 +0530

    recording video done

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+let transparentColor = "transparent";
+
+let recorder;
+var chunks = [];     // to store video data in chunks
 
 let constraints = {
     video: true,
-    audio: true,
-};
+    audio: true
+}
 
-// navigator is a global object which contains the info of browser
-// The MediaDevices.getUserMedia() method prompts the user for permission to use a media input
-// navigator.mediaDevices.getUserMedia(constraints)
-//     .then((stream) => {
-//         video.srcObject= stream;
+// navigator -> global, browser info
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+        video.srcObject = stream;
 
-//     })
-//     .catch(function (err) {
-        
-//     });
+        recorder = new MediaRecorder(stream);
+        // for recording
+        recorder.addEventListener('start', (e) => {    // when start new recording erasing the previous data
+            chunks = [];
+        })
+        recorder.addEventListener('dataavailable', (e) => {
+            chunks.push(e.data);
+        })
+        recorder.addEventListener('stop', (e) => {
+            // conversion of media chunks to video
+            var blob = new Blob(chunks, { 'type' : 'video/mp4' });
+            var videoURL = URL.createObjectURL(blob);
+
+            let a = document.createElement('a');
+            a.href = videoURL;
+            a.download = "stream.mp4";
+            a.click();
+        })
+    })
+
+recordBtnCont.addEventListener("click", (e) => {
+    if (!recorder) return;
+
+    recordFlag = !recordFlag;                       // same button will start and stop recording
+
+    if (recordFlag) {                               // start recoding
+        recorder.start();
+        recordBtn.classList.add("scale-record");
+        startTimer();
+    }
+    else {                                          // stop recoding
+        recorder.stop();
+        recordBtn.classList.remove("scale-record");
+        stopTimer();
+    }
+})
+
+
 
