4e3b38a9bdbf93a6a00b9d4d09c7556ed1d67e2d
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-09 12:15:22 +0530

    added timer and enhance ui

--- script.js
+++ script.js
@@ -58,3 +58,37 @@ recordBtnCont.addEventListener("click", (e) => {
 
 
 
+
+// for timer
+let timerID;
+let counter = 0; // Represents total seconds
+let timer = document.querySelector(".timer");
+function startTimer() {
+    timer.style.display = "block";
+    function displayTimer() {
+        let totalSeconds = counter;
+
+        let hours = Number.parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600; // remaining value
+
+        let minutes = Number.parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60; // remaining value
+
+        let seconds = totalSeconds;
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+        timer.innerText = `${hours}:${minutes}:${seconds}`;
+
+        counter++;
+    }
+
+    timerID = setInterval(displayTimer, 1000);
+}
+function stopTimer() {
+    clearInterval(timerID);
+    timer.innerText = "00:00:00";
+    timer.style.display = "none";
+}
