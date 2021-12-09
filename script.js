7f8d3e6e8a0b6f8f38aa5b1d6739529238bcadfb
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-09 10:11:35 +0530

    implemented mediaDevices for video capture

---
+++ script.js
@@ -0,0 +1,17 @@
+let video = document.querySelector("video");
+
+let constraints = {
+    video: true,
+    audio: true,
+};
+
+// navigator is a global object which contains the info of browser
+// The MediaDevices.getUserMedia() method prompts the user for permission to use a media input
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+        video.srcObject= stream;
+        
+    })
+    .catch(function (err) {
+        
+    });
